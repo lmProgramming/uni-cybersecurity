2271a8cb382173ca53e2d1c7eb518953418bd390
lmProgramming <mikikubs@gmail.com>
2025-02-22 14:55:46 +0100

    docs: added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cybersecurity
+
+Here lie just the task descriptions for each lab, in lab04 and lab05 are cool Python automatization scripts to make my life a bit easier
--- lab05/key_obfuscator.py
+++ lab05/key_obfuscator.py
@@ -6,25 +6,25 @@ bits_missing_list: list[int] = [4, 8, 12, 16, 20, 24, 32, 48]
 
 
 def obfuscate_key(key: str, bits_missing: int) -> list[str]:
-    hex_chars_missing = bits_missing // 4
-    key_length = len(key)
+    hex_chars_missing: int = bits_missing // 4
+    key_length: int = len(key)
 
     # beginning
-    key_beginning = '*' * hex_chars_missing + key[hex_chars_missing:]
+    key_beginning: str = '*' * hex_chars_missing + key[hex_chars_missing:]
 
     # middle
-    middle_start = (key_length - hex_chars_missing) // 2
-    key_middle = key[:middle_start] + '*' * \
+    middle_start: int = (key_length - hex_chars_missing) // 2
+    key_middle: str = key[:middle_start] + '*' * \
         hex_chars_missing + key[middle_start + hex_chars_missing:]
 
     # end
-    key_end = key[:-hex_chars_missing] + '*' * hex_chars_missing
+    key_end: str = key[:-hex_chars_missing] + '*' * hex_chars_missing
 
     # random
-    key_random = key
-    changes_left = hex_chars_missing
+    key_random: str = key
+    changes_left: int = hex_chars_missing
     while changes_left > 0:
-        random_index = random.randint(0, key_length - 1)
+        random_index: int = random.randint(0, key_length - 1)
         if key_random[random_index] != '*':
             changes_left -= 1
         else:
@@ -36,7 +36,7 @@ def obfuscate_key(key: str, bits_missing: int) -> list[str]:
 
 
 for bits_missing in bits_missing_list:
-    obfuscated_keys = obfuscate_key(key, bits_missing)
+    obfuscated_keys: list[str] = obfuscate_key(key, bits_missing)
     print(f"Bits missing: {bits_missing}")
     for obfuscated_key in obfuscated_keys:
         print(obfuscated_key)
